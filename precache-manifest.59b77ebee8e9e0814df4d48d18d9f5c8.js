528e1c1fe779583c491c4599f06adbde2646ad74
Abbas Ali <abbasali211@gmail.com>
2019-01-03 17:16:22 +0500

    Updates

---
+++ precache-manifest.59b77ebee8e9e0814df4d48d18d9f5c8.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "91a22847a03ea205e00a",
+    "url": "/react-cm/static/js/runtime~main.91a22847.js"
+  },
+  {
+    "revision": "cd8967add1af52bd5ac3",
+    "url": "/react-cm/static/js/main.cd8967ad.chunk.js"
+  },
+  {
+    "revision": "c071f76c5e88acf50eb9",
+    "url": "/react-cm/static/js/1.c071f76c.chunk.js"
+  },
+  {
+    "revision": "cd8967add1af52bd5ac3",
+    "url": "/react-cm/static/css/main.a7144fb5.chunk.css"
+  },
+  {
+    "revision": "c071f76c5e88acf50eb9",
+    "url": "/react-cm/static/css/1.8243d34b.chunk.css"
+  },
+  {
+    "revision": "84e237ae9c2f1685c678911ba81afc5a",
+    "url": "/react-cm/index.html"
+  }
+];
